5a803a7efc499075b36d02db653457d86c5be743
bright357122 <socool1668@gmail.com>
2018-09-26 16:14:56 +0800

    saliva_animate

---
+++ Assets/Scripts/Saliva.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saliva : MonoBehaviour {
+
+    public Vector3 init_pos;
+    public Rigidbody2D rigid;
+    public Vector2 velocity;
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = velocity;
+        init_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        if(Mathf.Abs(transform.position.x - init_pos.x) > 2.5)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
